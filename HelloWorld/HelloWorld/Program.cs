b32c88165c4b54cd38f18342498f86675adb301f
Sparshita14 <sparshitapc@gmail.com>
2024-01-22 21:24:25 +0530

    Second commit in Hello World

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,7 @@
        public static void Main(string[] args)
         {
         Console.WriteLine("Hello World from Sparshita");
+        Console.WriteLine("This is the second commit done on the same day");
         Console.ReadLine();
     }
    }
