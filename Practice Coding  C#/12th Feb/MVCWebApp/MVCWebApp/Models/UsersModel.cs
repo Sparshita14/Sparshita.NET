ae06ba3fe98bda2eba7090a62f1c7cc1dbe0df20
Sparshita14 <sparshitapc@gmail.com>
2024-02-12 21:06:19 +0530

    MVC Training session 12th Feb 24

---
+++ Practice Coding  C#/12th Feb/MVCWebApp/MVCWebApp/Models/UsersModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApp.Models
+{
+    public class UsersModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime LastModified { get; set; }
+
+        [Required]
+        public string ModifiedBy { get; set; }
+    }
+}
